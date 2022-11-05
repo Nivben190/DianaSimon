60058b8cfbc0cff75352d2aeedd3f3ef256f3160
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-11-05 10:34:44 +0200

    trying to upload to vercel

--- app/page.js
+++ app/page.js
@@ -1,8 +1,5 @@
 "use client";
-
 import React from "react";
-import { useState } from "react";
-
 const HomePage = () => {
   return (
     <h1>niv is the king</h1>
@@ -24,14 +21,13 @@ const HomePage = () => {
     //               }}
     //             ></div>
     //            </Link>
-                
+
     //           </Grid>
     //         );
     //       })}
     //     </Grid>
     //   </div>
     // </div>
-    
   );
 };
 
