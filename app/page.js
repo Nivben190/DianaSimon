755ee4c8be0e0cffb5b9a2d513d5aa530d9adc9e
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-11-05 11:10:19 +0200

    trying to upload to vercel6

--- app/page.js
+++ app/page.js
@@ -1,3 +1,40 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
-}
+"use client";
+
+import styles from "./index.module.css";
+import React from "react";
+import { useState } from "react";
+import Works from "./Works/page";
+import Content from "../components/Content";
+
+const HomePage = () => {
+  return (
+    // <div className={styles.mainDiv}>
+    //   <div className={styles.worksDiv}>
+    //     <Grid container align="center">
+    //       {Array.from(Array(50)).map((i,index) => {
+    //         return (
+    //           <Grid item sm={6} md={4} key={index}>
+    //            <Link href={`/works/${index}`}>
+    //            <div
+    //               id={index}
+    //               style={{
+    //                 width: "276px",
+    //                 height: "272px",
+    //                 background: "red",
+    //                 marginTop: "50px",
+    //                 cursor: "pointer",
+    //               }}
+    //             ></div>
+    //            </Link>
+                
+    //           </Grid>
+    //         );
+    //       })}
+    //     </Grid>
+    //   </div>
+    // </div>
+     <Works />
+  );
+};
+
+export default HomePage;
