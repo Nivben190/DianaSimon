fb85a03d5688a19cf7dc8ee11afc66d83c6194e8
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-11-05 11:13:02 +0200

    pushed to vercel with navbar and footer

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,14 @@
-
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </head>
+     <Navbar/>
     <body>{children}</body>
+    <Footer/>
   </html>
   )
 }
