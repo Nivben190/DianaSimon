374874ec47536308eae495aa1c92af659412d1d3
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-11-05 10:53:31 +0200

    trying to upload to vercel3

--- app/page.js
+++ app/page.js
@@ -1,37 +1,8 @@
-"use client";
-import React from "react";
-const HomePage = () => {
-  return (
-      <>
-        <h1>Home Page</h1>
-      </>
-      
-    // <div className={styles.mainDiv}>
-    //   <div className={styles.worksDiv}>
-    //     <Grid container align="center">
-    //       {Array.from(Array(50)).map((i,index) => {
-    //         return (
-    //           <Grid item sm={6} md={4} key={index}>
-    //            <Link href={`/works/${index}`}>
-    //            <div
-    //               id={index}
-    //               style={{
-    //                 width: "276px",
-    //                 height: "272px",
-    //                 background: "red",
-    //                 marginTop: "50px",
-    //                 cursor: "pointer",
-    //               }}
-    //             ></div>
-    //            </Link>
 
-    //           </Grid>
-    //         );
-    //       })}
-    //     </Grid>
-    //   </div>
-    // </div>
+ export const HomePage = () => {
+  return (
+      <div>
+        <h1>Home Page</h1>
+      </div>  
   );
 };
-
-export default HomePage;
