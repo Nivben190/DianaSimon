88077266f7607ff7962e7108e446f7ab594ce998
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-11-14 21:59:10 +0200

    added some meta tags

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <title>Diana Shimon Gallery Website</title>
+      <meta name="descreption" content="diana shimon gallery website, desplay her work and some info about her" />
+
        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous">
    </link>
